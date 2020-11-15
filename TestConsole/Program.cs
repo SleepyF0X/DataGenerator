de9114c71ab26551d3d1c7f0f60214b1593f6f79
SleepyF0X <kotohomka@gmail.com>
2020-11-16 01:51:34 +0200

    Demo :)

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -26,18 +26,31 @@ namespace TestConsole
                 .ForProperty(s => s.Email, opt => opt.IsEmail())
                 .ForProperty(s => s.RecordBook, opt => opt.IsRndInt(1000, 9999))
                 .ForProperty(s => s.Info, opt => opt.IsRndText(10));
-            var models = new ModelBuilder<Student>(config.WithParent(s => s.GroupId, s => s.Group, g => g.Id, group)).Build(100);
-            var models2 = new ModelBuilder<Student>(config.WithParent(s => s.GroupId, s => s.Group, g => g.Id, group2)).Build(100);
+            var models = new ModelBuilder<Student>(config.WithParent(s => s.GroupId, s => s.Group, g => g.Id, group)).Build(1);
+            var models2 = new ModelBuilder<Student>(config.WithParent(s => s.GroupId, s => s.Group, g => g.Id, group2)).Build(1);
             foreach (var model in models2)
             {
-                Console.WriteLine("Id: " + model.Id + Environment.NewLine + 
-                                  "Name: " + model.Name + Environment.NewLine + 
+                Console.WriteLine("Id: " + model.Id + Environment.NewLine +
+                                  "Name: " + model.Name + Environment.NewLine +
                                   "Surname: " + model.Surname + Environment.NewLine +
                                   "Age: " + model.Age + Environment.NewLine +
                                   "Email: " + model.Email + Environment.NewLine +
                                   "Record Book number: " + model.RecordBook + Environment.NewLine +
                                   "Info: " + model.Info + Environment.NewLine +
-                                  "GroupId: " + model.GroupId+ Environment.NewLine +
+                                  "GroupId: " + model.GroupId + Environment.NewLine +
+                                  "Group Name: " + model.Group?.Name + Environment.NewLine +
+                                  "___________________________________________________________________");
+            }
+            foreach (var model in models)
+            {
+                Console.WriteLine("Id: " + model.Id + Environment.NewLine +
+                                  "Name: " + model.Name + Environment.NewLine +
+                                  "Surname: " + model.Surname + Environment.NewLine +
+                                  "Age: " + model.Age + Environment.NewLine +
+                                  "Email: " + model.Email + Environment.NewLine +
+                                  "Record Book number: " + model.RecordBook + Environment.NewLine +
+                                  "Info: " + model.Info + Environment.NewLine +
+                                  "GroupId: " + model.GroupId + Environment.NewLine +
                                   "Group Name: " + model.Group?.Name + Environment.NewLine +
                                   "___________________________________________________________________");
             }
